260c2e05eb40971899384baedf4a47cbd3b842f7
asd412id <asdar.binsyam@gmail.com>
2024-05-11 23:44:08 +0800

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,157 @@
+import { Box, Card, CardContent, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from "@mui/material"
+import { CalculatedRates, SpeedRates, calculateRates, convertToString, rateLimit } from "./utils/burustCalculation";
+import { ChangeEvent, useEffect, useState } from "react";
+import { CheckCircle } from "@mui/icons-material";
+import logo from './assets/logo.jpeg';
+
+function App() {
+  const [form, setForm] = useState<SpeedRates>({
+    speedUpload: '512K',
+    speedDownload: '1M',
+    bonusUpload: '1M',
+    bonusDownload: '2M',
+    bonusUploadDuration: 8,
+    bonusDownloadDuration: 8
+  })
+
+  const [rates, setRates] = useState<Partial<CalculatedRates>>();
+  const [rateString, setRateString] = useState('')
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(rateString);
+      setCopied(true);
+    } catch (err) {
+      console.error('Failed to copy text:', err);
+    } finally {
+      setTimeout(() => setCopied(false), 2000);
+    }
+  };
+
+  useEffect(() => {
+    calculateRate()
+  }, [form])
+
+  const calculateRate = () => {
+    const calculate = calculateRates(form)
+    setRates(calculate)
+    setRateString(rateLimit(calculate))
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value.toUpperCase() })
+  }
+
+  return (
+    <Box className="flex flex-col items-center justify-center bg-gradient-to-br from-slate-100 to-slate-300 p-3" sx={{ minHeight: '100dvh' }}>
+      <div className="flex w-full lg:max-w-screen-xl">
+        <Card elevation={5} className="w-full">
+          <CardContent sx={{ p: 5 }}>
+            <div className="flex flex-col gap-10">
+              <div className="flex items-center justify-center gap-3">
+                <img src={logo} alt="logo" className="w-16 rounded-lg" />
+                <Typography variant="h3" align="center" className="!leading-none !p-0 !m-0">Mikrotik Burst Limit Kalkulator</Typography>
+              </div>
+              <div className="flex flex-col md:flex-row gap-10 w-full">
+                <div className="flex flex-col gap-5 w-full">
+                  <Typography className="!text-3xl" align="center">KONFIGURASI</Typography>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell className="!border !border-gray-300" align="center">TARGET UPLOAD</TableCell>
+                        <TableCell className="!border !border-gray-300" align="center">TARGET DOWNLOAD</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth variant="outlined" label="KECEPATAN UPLOAD (K/M)" value={form.speedUpload} name="speedUpload" onChange={handleChange} />
+                        </TableCell>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth variant="outlined" label="KECEPATAN DOWNLOAD (K/M)" value={form.speedDownload} name="speedDownload" onChange={handleChange} />
+                        </TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth variant="outlined" label="BONUS UPLOAD (K/M)" value={form.bonusUpload} name="bonusUpload" onChange={handleChange} />
+                        </TableCell>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth variant="outlined" label="BONUS DOWNLOAD (K/M)" value={form.bonusDownload} name="bonusDownload" onChange={handleChange} />
+                        </TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth type="number" variant="outlined" label="DURASI BONUS UPLOAD (DETIK)" value={form.bonusUploadDuration} name="bonusUploadDuration" onChange={handleChange} />
+                        </TableCell>
+                        <TableCell className="!border !border-gray-300">
+                          <TextField fullWidth type="number" variant="outlined" label="DURASI BONUS DOWNLOAD (DETIK)" value={form.bonusDownloadDuration} name="bonusDownloadDuration" onChange={handleChange} />
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </div>
+                <div className="flex flex-col gap-5 w-full">
+                  <Typography className="!text-3xl" align="center">HASIL KALKULASI</Typography>
+                  <div className="flex flex-col gap-1">
+                    <Typography>PENGATURAN QUEUE</Typography>
+                    <Table size="small">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300" align="center">PENGATURAN</TableCell>
+                          <TableCell className="!border !border-gray-300" align="center">UPLOAD</TableCell>
+                          <TableCell className="!border !border-gray-300" align="center">DOWNLOAD</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Max Limit</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.maxUploadLimit ?? 0)}</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.maxDownloadLimit ?? 0)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Burst Limit</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstUploadLimit ?? 0)}</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstDownloadLimit ?? 0)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Burst Threshold</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstUploadThreshold ?? 0)}</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstDownloadThreshold ?? 0)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Burst Time</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstTimeUpload ?? 0)}</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.burstTimeDownload ?? 0)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Limit At</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.limitAtUpload ?? 0)}</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300">{convertToString(rates?.limitAtDownload ?? 0)}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <Typography>PENGATURAN HOTSPOT ATAU PPPOE</Typography>
+                    <Table size="small">
+                      <TableBody>
+                        <TableRow>
+                          <TableCell className="!border !border-gray-300">Rate Limit</TableCell>
+                          <TableCell align="center" className="!border !border-gray-300 !bg-sky-100 cursor-pointer hover:!bg-sky-200" title="Klik untuk menyalin" onClick={handleCopy}>{rateString}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                    {copied && <span className="flex gap-1 items-center justify-end text-emerald-500"><CheckCircle fontSize="small" /> Berhasil disalin</span>}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/utils/burustCalculation.ts
@@ -0,0 +1,107 @@
+export interface SpeedRates {
+  speedUpload: string;
+  speedDownload: string;
+  bonusUpload: string;
+  bonusDownload: string;
+  bonusUploadDuration: number;
+  bonusDownloadDuration: number;
+}
+
+export interface CalculatedRates {
+  maxUploadLimit: number;
+  maxDownloadLimit: number;
+  burstUploadLimit: number;
+  burstDownloadLimit: number;
+  burstUploadThreshold: number;
+  burstDownloadThreshold: number;
+  burstTimeUpload: number;
+  burstTimeDownload: number;
+  limitAtUpload: number;
+  limitAtDownload: number;
+}
+
+function parseSpeedValue(value: string): number {
+  const parsedValue = parseInt(value, 10);
+  const unit = value.replace(/\d+/g, '');
+
+  switch (unit.toLowerCase()) {
+    case 'k':
+      return parsedValue;
+    case 'm':
+      return parsedValue * 1000;
+    case 'g':
+      return parsedValue * 1000 * 1000;
+    default:
+      return parsedValue;
+  }
+}
+
+function getBurstThreshold(maxLimit: number): number {
+  return Math.round(maxLimit * 3 / 4);
+}
+
+function getBurstTime(burstLimit: number, interval: number, burstThreshold: number): number {
+  return Math.round((burstLimit * interval) / burstThreshold);
+}
+
+export function convertToString(number: number): string {
+  const numberString = number.toString();
+  const length = numberString.length;
+
+  if (length < 4) {
+    return `${numberString}K`;
+  } else if (length < 8) {
+    if (numberString.slice(-3) === "000") {
+      return `${numberString.slice(0, -3)}M`
+    }
+    return `${numberString}K`
+  } else {
+    if (numberString.slice(-3) === "000") {
+      return `${numberString.slice(0, -3)}G`
+    }
+    return `${numberString}M`
+  }
+}
+
+export function rateLimit(rates: CalculatedRates): string {
+  const maxUpload = convertToString(rates.maxUploadLimit)
+  const maxDownload = convertToString(rates.maxDownloadLimit)
+  const burstUploadLimit = convertToString(rates.burstUploadLimit)
+  const burstDownloadLimit = convertToString(rates.burstDownloadLimit)
+  const burstUploadThreshold = convertToString(rates.burstUploadThreshold)
+  const burstDownloadThreshold = convertToString(rates.burstDownloadThreshold)
+  const burstUploadTime = rates.burstTimeUpload
+  const burstDownloadTime = rates.burstTimeDownload
+  const uploadLimitAt = convertToString(rates.limitAtUpload)
+  const downloadLimitAt = convertToString(rates.limitAtDownload)
+
+  return `${maxUpload}/${maxDownload} ${burstUploadLimit}/${burstDownloadLimit} ${burstUploadThreshold}/${burstDownloadThreshold} ${burstUploadTime}/${burstDownloadTime} 8 ${uploadLimitAt}/${downloadLimitAt}`
+}
+
+export function calculateRates(rates: SpeedRates): CalculatedRates {
+  const { speedUpload, speedDownload, bonusUpload, bonusDownload, bonusUploadDuration, bonusDownloadDuration } = rates;
+
+  const maxUploadLimit = parseSpeedValue(speedUpload);
+  const maxDownloadLimit = parseSpeedValue(speedDownload);
+  const burstUploadLimit = parseSpeedValue(bonusUpload);
+  const burstDownloadLimit = parseSpeedValue(bonusDownload);
+  const burstUploadThreshold = getBurstThreshold(parseSpeedValue(speedUpload));
+  const burstDownloadThreshold = getBurstThreshold(parseSpeedValue(speedDownload));
+  const burstTimeUpload = getBurstTime(parseSpeedValue(bonusUpload), bonusUploadDuration, burstUploadThreshold);
+  const burstTimeDownload = getBurstTime(parseSpeedValue(bonusDownload), bonusDownloadDuration, burstDownloadThreshold);
+  const limitAtUpload = Math.round(maxUploadLimit * 0.3);
+  const limitAtDownload = Math.round(maxDownloadLimit * 0.3);
+
+  return {
+    maxUploadLimit,
+    maxDownloadLimit,
+    burstUploadLimit,
+    burstDownloadLimit,
+    burstUploadThreshold,
+    burstDownloadThreshold,
+    burstTimeUpload,
+    burstTimeDownload,
+    limitAtUpload,
+    limitAtDownload,
+  };
+}
